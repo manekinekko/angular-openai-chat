d1ae37c3b6e95a35844c6767231ac59d8f77acc3
Wassim Chegham <github@wassim.dev>
2023-02-23 09:46:42 +0100

    chore: add basic UI foundation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,162 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'openai-root',
+  selector: "openai-root",
   standalone: true,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
 })
 export class AppComponent {
-  title = 'openai-chat';
+  conversation = [
+    {
+      text: "Hello, how are you?",
+      from: "user",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "Hello, how are you?",
+      from: "user",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "Hello, how are you?",
+      from: "user",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "Hello, how are you?",
+      from: "user",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+    },
+    {
+      text: "Hello, how are you?",
+      from: "user",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "Hello, how are you?",
+      from: "user",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "Hello, how are you?",
+      from: "user",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "Hello, how are you?",
+      from: "user",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "Hello, how are you?",
+      from: "user",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+    },
+    {
+      text: "Hello, how are you?",
+      from: "user",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+    {
+      text: "I'm fine, thanks. How are you?",
+      from: "bot",
+      date: new Date(),
+    },
+  ];
+
+  async send(text: string) {
+    console.log(text);
+  }
 }
