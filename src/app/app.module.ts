51a905217178546b7f951544796875cd2dbcdb99
Wassim Chegham <github@wassim.dev>
2023-02-23 08:53:47 +0100

    chore: move to angular standalone mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule()
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserAnimationsModule)]
+})
   .catch(err => console.error(err));
