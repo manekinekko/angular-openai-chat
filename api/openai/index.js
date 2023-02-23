18401b0d1bda3271053145fea6771b0097e77d86
Wassim Chegham <github@wassim.dev>
2023-02-23 10:39:01 +0100

    feat: connect to openai endpoint

--- api/openai/index.js
+++ api/openai/index.js
@@ -1,13 +1,39 @@
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+
+const conversation_history = [
+  "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.",
+  "",
+  "Human: Hello, who are you?",
+  "AI: I am an AI created by OpenAI. How can I help you today?",
+];
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  let { prompt } = req.body;
+  
+  if (!prompt) {
+    prompt = conversation_history.join("");
+  } else {
+    prompt = conversation_history.join("\n") + "\nHuman: " + prompt + "\nAI:";
+  }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt,
+    temperature: 0.9,
+    max_tokens: 150,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.6,
+    stop: [" Human:", " AI:"],
+  });
+
+  context.res = {
+    body: completion.data.choices[0],
+  };
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from "@angular/core";
 import { MatCardModule } from "@angular/material/card";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
-import { MatProgressBarModule } from "@angular/material/progress-bar";
-import { MatDividerModule } from "@angular/material/divider";
+import {
+  MatProgressBarModule,
+  ProgressBarMode,
+} from "@angular/material/progress-bar";
+import { MatDivider, MatDividerModule } from "@angular/material/divider";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { CommonModule } from "@angular/common";
+
+type Message = {
+  text: string;
+  from: "user" | "bot";
+  date: Date;
+};
 
 @Component({
   selector: "openai-root",
@@ -25,138 +34,53 @@ import { CommonModule } from "@angular/common";
   ],
 })
 export class AppComponent {
-  conversation = [
-    {
-      text: "Hello, how are you?",
-      from: "user",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "Hello, how are you?",
-      from: "user",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "Hello, how are you?",
-      from: "user",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "Hello, how are you?",
-      from: "user",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-    },
-    {
-      text: "Hello, how are you?",
-      from: "user",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "Hello, how are you?",
-      from: "user",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "Hello, how are you?",
-      from: "user",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "Hello, how are you?",
-      from: "user",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "Hello, how are you?",
-      from: "user",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-    },
-    {
-      text: "Hello, how are you?",
-      from: "user",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-    {
-      text: "I'm fine, thanks. How are you?",
-      from: "bot",
-      date: new Date(),
-    },
-  ];
+  conversation: Message[] = [];
+  loadingStatus: ProgressBarMode = "determinate";
 
-  async send(text: string) {
-    console.log(text);
+  @ViewChildren("conversationRef", {
+    read: ElementRef,
+  })
+  conversationRef!: QueryList<ElementRef<HTMLElement>>;
+
+  ngAfterViewInit(): void {
+    this.conversationRef.changes
+        .subscribe(() => {
+          this.conversationRef.last.nativeElement.scrollIntoView();
+        });
+}
+
+
+  async send(prompt: HTMLInputElement) {
+    this.loadingStatus = "buffer";
+
+    this.updateConversation({
+      text: prompt.value,
+      from: "user",
+      date: new Date(),
+    });
+
+    const response = await fetch("/api/openai", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: prompt.value,
+      }),
+    });
+    const { text } = await response.json();
+    this.updateConversation({
+      text,
+      from: "bot",
+      date: new Date(),
+    });
+
+    prompt.value = "";
+
+    this.loadingStatus = "determinate";
+  }
+
+  updateConversation(message: Message) {
+    this.conversation.push(message);
   }
 }
